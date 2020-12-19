ea9f5a8674743cce2975ff97187802192c374d1a
Lee-YuGyeong <60072190+Lee-YuGyeong@users.noreply.github.com>
2020-12-19 20:37:39 +0900

    login

--- app/src/main/java/com/example/timestamp/login/LoginActivity.java
+++ app/src/main/java/com/example/timestamp/login/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -76,12 +77,14 @@ public class LoginActivity extends AppCompatActivity {
                         final String userID = jsonObject.getString("userID");
                         final String userPass = jsonObject.getString("userPassword");
 
+                        getName(userID);
+
                         AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                         builder.setMessage("자동로그인에 등록하시겠습니까?");
                         builder.setPositiveButton("예",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
-                                        InLogDate(userID, userPass, true);
+                                        InputLogData(userID, userPass, true);
                                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                         startActivity(intent);
                                     }
@@ -89,7 +92,7 @@ public class LoginActivity extends AppCompatActivity {
                         builder.setNegativeButton("아니오",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
-                                        InLogDate(userID, userPass, false);
+                                        InputLogData(userID, userPass, false);
                                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                         startActivity(intent);
                                     }
@@ -113,7 +116,27 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void InLogDate(String id, String password, boolean auto) { // SharedPreferences에 값 저장.
+    public void getName(String userID) {
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String getUserName = jsonObject.getString("userName");
+
+                    InputNameData(getUserName);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        GetNameRequest getNameRequest = new GetNameRequest(userID, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+        queue.add(getNameRequest);
+    }
+
+    public void InputLogData(String id, String password, boolean auto) { // SharedPreferences에 값 저장.
 
         SharedPreferences sharedPreferences = getSharedPreferences("mine", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -121,6 +144,15 @@ public class LoginActivity extends AppCompatActivity {
         editor.putString("userPassword", password);
         editor.putBoolean("auto", auto);
         editor.commit();    //최종 커밋. 커밋을 해야 저장이 된다.
+
+    }
+
+    public void InputNameData(String name) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("mine", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("userName", name);
+        editor.commit();
 
     }
 
